45270cf3fa3740620321c87ca34bafb7df41ef9a
Ashish Bajpai <ashishvirgo12@gmail.com>
2026-07-31 14:39:32 +0530

    demo fetch

---
+++ demo5.js
@@ -0,0 +1,14 @@
+
+export const sum=(...num)=>{
+    const s=num.reduce((s,i)=>(s+i));
+    return s;
+}
+
+export const add=(...num)=>{
+    let s=0;
+    for(let i of num){
+        s=s+i;
+    }
+    return s;
+}
+// export {sum,add};
---
+++ test.js
@@ -0,0 +1,10 @@
+import {sum,add} from "./demo5.js";
+console.log("Sum=",sum(1));
+console.log("Sum=",sum(12,34));
+console.log("Sum=",sum(101,209,345));
+console.log("Sum=",sum(13,34,56,78,89));
+
+console.log("add=",add(2));
+console.log("add=",add(32,34));
+console.log("add=",add(151,209,345));
+console.log("add=",add(31,34,56,78,89));
